bbfbc215a787c1e15f6e2cbd7314bef687b2a52f
ladypinetree <maureenpine@gmail.com>
2017-02-02 10:51:35 -0500

    Puts in lesson responses

--- bin/hash_list.rb
+++ bin/hash_list.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 # hash as enumerable code along
+
+alpha = ('a'..'z').map(&:to_sym).freeze
+hash = {}
+
+(1..26).each_with_index do |number, index|
+   hash[alpha[index]] = number
+end
+
+puts "hash is #{hash}"
+
+less_than_twenty_seven = hash.all? do |_key, value|
+  value < 27
+end
+
+puts "less than 27 letters in the alphabet? #{less_than_twenty_seven}"
+
+# _ before a variable like _key tells ruby you might not use it
+#hash.valus
+#hash.keys
--- bin/range_list.rb
+++ bin/range_list.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 # range as enumerable code along
+
+ints = 0..10
+
+puts "ints range is #{ints}"
+puts "ints max is #{ints.max}" #uses each to iterate
+puts "ints min is #{ints.min}"
+puts "ints max two elements are #{ints.max(2)}"
+
+alpha = 'a'..'z'
+
+puts "alpha range is #{alpha}" #.to_a will print all strings in range
+puts "alpha max is #{alpha.max}"
+puts "alpha min is #{alpha.min}"
+
+every_third_alpha = alpha.group_by do |a|
+  (a.ord % 3).zero?
+end
+
+puts "every third alpha is #{every_third_alpha}"
